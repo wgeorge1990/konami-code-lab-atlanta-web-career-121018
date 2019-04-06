19f69d14641182da80882c73d0123e2d8b6eaa29
wgeorge1990 <williameverettgeorge@icloud.com>
2019-04-05 23:53:03 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,16 @@ const codes = [
   "a"
 ];
 
+
+
 function init() {
-  // your code here
+  let  index = 0
+  document.body.addEventListener("keydown", function(event) {
+    let key = event.key
+    index = (codes[index] === key) ? (index = index + 1 || ++index) : 0
+    if(index === codes.length) {
+      window.alert('You figured it out');
+      index = 0
+    }
+  })
 }
